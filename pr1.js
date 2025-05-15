2d59b110dd2ead6aa42c35ae08e199d83e99cf9d
Kiram102 <amandaazhar8@gmail.com>
2025-05-15 14:54:21 +0700

    sql custom

--- pr1.js
+++ pr1.js
@@ -2,8 +2,13 @@
 const { error } = require("console");
 const express = require("express");
 const { get } = require("http");
+const db = require("./connection");
 const app = express();
 const port = 5000;
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
 
 app.get("/ucap", (req, res) => {
   res.send("Selamat Siang Dari Server");
@@ -16,7 +21,7 @@ app.get("/", (req, res) => {
 app.get("/data-diri",(req,res)=>{
     res.json({
         nama:"Kiki Ramdani",
-        usia: 7,
+        usia: 17,
         kota:"Cibaduyut",
     });
 });
@@ -38,7 +43,64 @@ if(data){
 }
 });
 
+// add //
 
+app.post('/add-produk',(req,res)=>{
+    const {id_produk,produk,kategori,harga_per_kg} = req.body;
+    const sql = `INSERT INTO produk (id_produk,produk,kategori,harga_per_kg) VALUES (
+    ${id_produk},"${produk}","${kategori}",${harga_per_kg})`;
+    
+    db.query(sql,(error,result)=>{
+        if(error) throw error;
+    });
+    return res.status(200).send("ok");
+});
+
+// add //
+
+
+// update //
+
+app.put("/update/:id",(req,res)=>{
+    const idproduk = req.params.id;
+    const {produk,kategori,harga_per_kg} = req.body;
+    const sql = `UPDATE produk SET produk = "${produk}", kategori = "${kategori}", harga_per_kg = ${harga_per_kg}
+    WHERE id_produk = ${idproduk}`;
+
+      db.query(sql,(error,result)=>{
+        if(error) throw error;
+    });
+    return res.status(200).send("Data Telah Diubah");
+});
+
+
+// update //
+
+// show //
+
+app.get("/find/:id", (req, res) => {
+  const idproduk = req.params.id;
+  const sql = `SELECT * FROM produk WHERE id_produk = '${idproduk}'`;
+
+  db.query(sql, (error, result) => {
+    res.json(result);
+  });
+});
+
+// show //
+
+// delete //
+app.delete("/delete/:id",(req,res)=>{
+    const idproduk = req.params.id;
+  const sql = `DELETE  FROM produk WHERE id_produk = '${idproduk}'`;
+
+  db.query(sql, (error, result) => {
+    res.json(result);
+  });
+   return res.status(200).send("Data Telah Dihapus");
+});
+
+// delete//
 
 
 app.listen(port, () => {
